ff6e6cbbe1d976b99c50f20caa1d6e3f3f6b9814
Tapan Kumar Patra <tapan.patra@aveva.com>
2025-07-25 16:20:58 +0530

    Commit Day 4 Code.

---
+++ csharp_mastery/100DaysOfCode_CSharp_Automation/Day4_CalculatorApp/Day4_CalculatorApp/Calculator.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4_CalculatorApp
+{
+    public class Calculator
+    {
+        private ILogger<Calculator> _logger;
+        public Calculator(ILogger<Calculator> logger)
+        {
+            _logger = logger;
+        }
+
+        public double Add(int a, int b)
+        {
+            double result = a + b;
+            _logger.LogInformation("Add {a}, {b} = {result}", a, b, result);
+            return a + b;
+        }
+    }
+}
---
+++ csharp_mastery/100DaysOfCode_CSharp_Automation/Day4_CalculatorApp/Day4_CalculatorApp/CalculatorAppTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Logging;
+using Serilog.Extensions.Logging;
+using Serilog;
+using Serilog.Core;
+
+namespace Day4_CalculatorApp
+{
+    public class CalculatorAppTests
+    {
+        private Calculator _calc;
+        [SetUp]
+        public void Setup()
+        {
+            Log.Logger = new LoggerConfiguration()
+             .MinimumLevel.Information()
+             .WriteTo.Console()
+             .WriteTo.File("logs/test_log.txt", rollingInterval: RollingInterval.Day)
+             .CreateLogger();
+
+            ILoggerFactory loggerFactory = new SerilogLoggerFactory(Log.Logger);
+            ILogger<Calculator> logger = loggerFactory.CreateLogger<Calculator>();
+
+            _calc = new Calculator(logger);
+        }
+
+        [Test]
+        public void Add_ValidInputs_ReturnsSum()
+        {
+            var result = _calc.Add(5, 3);
+            Assert.That(result, Is.EqualTo(8));
+        }
+    }
+}
